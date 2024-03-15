0e1c6a595a5849e6b8af9daedd94ff31975b6aa2
CSJasA <140838212+CSJasA@users.noreply.github.com>
2024-03-14 23:49:34 -0400

    Door+Key Mechanic
    Added a pick up key and door system;
    need to pick up the key before being able to open the door

---
+++ Door.gd
@@ -0,0 +1,23 @@
+extends Node2D
+var can_open_door = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite2D.play("Closed")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_interaction_vinicity_area_entered(area):
+	if can_open_door:
+		$AnimatedSprite2D.play("Open")
+
+
+func _on_interaction_vinicinty_area_exited(area):
+	$AnimatedSprite2D.play("Closed")
+
+func _on_cat_key_collected():
+	can_open_door = true
