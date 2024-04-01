ba5f046399c9532930c2c9a43de1690df6fa077e
CSJasA <140838212+CSJasA@users.noreply.github.com>
2024-03-31 20:03:58 -0400

    Level_2 and Changing Levels
    Completed;
    - added level 2 (only needs the collision with tile maps)
    - switch between levels

--- Door.gd
+++ Door.gd
@@ -4,6 +4,8 @@ var can_open_door = false
 var keys = []
 var keys_collected = 0
 
+signal leave
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play("Closed")
@@ -13,12 +15,12 @@ func _ready():
 func _process(delta):
 	if (keys.size() == keys_collected):
 		can_open_door = true
+	if can_open_door and Input.is_action_just_pressed("interact"):
+		leave.emit()
 
 func _on_interaction_vinicity_area_entered(area):
 	if can_open_door:
 		$AnimatedSprite2D.play("Open")
-		if Input.is_action_just_pressed("interact"):
-			print("move to next level")
 
 func _on_key_key_collected():
 	keys_collected += 1
