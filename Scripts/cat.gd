6414fa2df0cece9506ef61e5a53fa36b48fc1e18
Disen1 <98978217+Disen1@users.noreply.github.com>
2024-04-07 20:05:57 -0400

    changed movement(still need fixing)

--- Scripts/cat.gd
+++ Scripts/cat.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
 @export var speed= 400 #speed
-@export var gravity = 20#gravit
+@export var GRAVITY = 20#gravit
 @export var jump = 600 #jump force
 @export var termVol =1000#terminal velocity
+@export var acc = 50
+var jumped = false
+
+@onready var axis = Vector2.ZERO
+
 var health = 9
 signal hit(health)
 signal dead()
@@ -14,63 +19,55 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var jumpRight=0
-	var jumpLeft=0
-	var flip
+	#gravity
+	velocity.y += GRAVITY
+	#base movement
 	
-	
-	if !is_on_floor(): #when the player isn't on the floor, have gravity pull them down
-		velocity.y += gravity
 		
-	#Wall Sl
-	if(is_on_wall()):
-		termVol = 100
+	#wall sliding
+	if (is_on_wall()):
+		termVol =100
 	else:
 		termVol=1000
-	
-	#checks if moving left
-	if (Input.is_action_just_pressed("walk_left")):
-		flip = true
-	else:
-		flip = false
-	#currently only allows the player to jump when it's on a platform, and after a certain amount of time(cooldown)
-	if (Input.is_action_just_pressed("jump") and (is_on_floor() or is_on_wall())):
-		$AnimatedSprite2D.play("jump")
-		velocity.y = -jump
-		$jump_timer.start()
-		$Jump.play()
-		$AnimatedSprite2D.flip_h = flip
+	if(velocity.y>termVol):
+		velocity.y = termVol
 		
-	elif (Input.is_action_just_pressed("walk_right") or Input.is_action_just_pressed("walk_left")):
-		$AnimatedSprite2D.flip_h = flip
-		$AnimatedSprite2D.play("run")
+	#player control and info
+	movement()
+	jumping()
+	if(get_input_direction()>0):
+		$AnimatedSprite2D.flip_h=false
 	else:
-		$AnimatedSprite2D.play("idle")
-	
-#	#so you dont fall too fast
-	if(velocity.y>= termVol):
-		velocity.y=termVol
-	$Label.set_text("gravity: " + str(gravity)+
-					"\nspeed: " + str(speed)+
-					"\ntermVol: " + str(termVol)+
-					"\njump: " + str(jump)+
-					"\nHealth: " + str(health))
-	
-	var horizontal_direction = Input.get_axis("walk_left","walk_right")
-	velocity.x = speed *horizontal_direction+jumpLeft+jumpRight
-	move_and_slide() #moves the player
-	
-	if (Input.is_action_just_pressed("interact")):
-		for body in $"Intereaction_Vicinity".get_overlapping_areas():
-			if body.is_in_group("keys"):
-				body.queue_free()
-	
-
+		$AnimatedSprite2D.flip_h=true
+	move_and_slide()
+	interact()
+	debugInfo()
 #to add a cooldown on jumping so the player can't spam jump
 func _on_jump_timer_timeout():
+	
 	$jump_timer.stop()
-
-
+	
+func get_input_direction():
+	var input_dir = Vector2.ZERO
+	input_dir = (Input.get_action_strength("walk_right") -Input.get_action_strength("walk_left"))
+	return input_dir
+func movement():
+	$AnimatedSprite2D.play("run")
+	velocity.x = get_input_direction() *speed
+	if(get_input_direction() ==0 and not jumped):
+		$AnimatedSprite2D.play("idle")
+		
+func interact():
+	if (Input.is_action_just_pressed("interact")):
+			for body in $"Intereaction_Vicinity".get_overlapping_areas():
+				if body.is_in_group("keys"):
+					body.queue_free()
+func debugInfo():
+	$Label.set_text("gravity: " + str(GRAVITY)+
+				"\nspeed: " + str(speed)+
+				"\ntermVol: " + str(termVol)+
+				"\njump: " + str(jump)+
+				"\nHealth: " + str(health))
 func _on_area_2d_area_entered(area):
 	if(area.name=="EnemyColision" or area.name=="WaterArea"):
 		health -=1
@@ -81,6 +78,13 @@ func _on_area_2d_area_entered(area):
 		if area.name=="Water":
 			velocity.y = -1500	
 	print("hit "+ area.name)
-
+func jumping():
+	if(Input.is_action_just_pressed("jump")):
+		jumped = true
+		$AnimatedSprite2D.play("jump")
+		if(is_on_floor()):
+			velocity.y=-jump
+		elif(is_on_wall()):
+			velocity.y=-jump
 func setHealth(value):
 	health = value
